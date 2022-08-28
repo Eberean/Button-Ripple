0aec675ebb6e8facdaf01bc46b227dc8c9fc26fe
Chidiebere John <chidieberejohn17@gmail.com>
2022-08-29 00:51:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const buttonsEl = document.querySelectorAll('.ripple')
+
+buttonsEl.forEach(button => {
+  button.addEventListener('click', function (e) {
+    const x = e.clientX
+    const y = e.clientY
+    // console.log(x, y);
+
+    const buttonTop = e.target.offsetTop
+    const buttonLeft = e.target.offsetLeft
+    // console.log(buttonTop, buttonLeft);
+
+    const xInside = x - buttonLeft
+    const yInside = y - buttonTop
+    // console.log(xInside, yInside);
+
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = yInside + 'px'
+    circle.style.left = xInside + 'px'
+
+    this.appendChild(circle)
+
+    setTimeout(() => circle.remove(), 500)
+  })
+})
